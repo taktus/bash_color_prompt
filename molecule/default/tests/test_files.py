3026d85a15c597f4b543cfc66eb06a04657cb133
Marek Krasnowski <marek@krasnowski.eu>
2020-03-25 12:11:57 +0100

    role files

---
+++ molecule/default/tests/test_files.py
@@ -0,0 +1,20 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+@pytest.mark.parametrize('file, content', [
+  ('/home/test_yes/.bashrc', 'force_color_prompt=yes'),
+  ('/home/test_no/.bashrc', '#force_color_prompt=yes'),
+  ('/home/test_no/.bashrc', r'033\[01;32m'),
+  ('/home/test_yes/.bashrc', r'033\[01;33m'),
+])
+
+def test_files(host, file, content):
+    file = host.file(file)
+
+    assert file.exists
+    assert file.contains(content)
---
+++ tests/test_color_prompt.py
@@ -0,0 +1,22 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+def test_sudo_is_installed(host):
+    sudo = host.package("sudo")
+    assert sudo.is_installed
+
+@pytest.mark.parametrize('file, content', [
+  ("/home/test_yes/.bashrc", "force_color_prompt=yes"),
+  ("/home/test_no/.bashrc", "#force_color_prompt=yes")
+])
+
+def test_files(host, file, content):
+    file = host.file(file)
+
+    assert file.exists
+    assert file.contains(content)
